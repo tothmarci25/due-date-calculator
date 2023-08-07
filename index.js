f877f20c152ff6c1a7349286a19826f4b50a1859
tothmarci25 <tothmarci25@gmail.com>
2023-08-08 01:36:36 +0200

    Add due date calculation

---
+++ index.js
@@ -0,0 +1,79 @@
+
+const DAILY_WORKING_HOURS = 8;
+const DAYS_IN_WEEK = 7;
+const END_OF_WORK = 17;
+const DAILY_NON_WORKING_HOURS = 16;
+
+
+export const calculateDueDate = (submitDate, turnaround) => {
+    validateArguments(submitDate, turnaround)
+
+    let resultDate = addWorkingDays(submitDate, turnaround)
+    resultDate = addRemainingWorkingHours(resultDate, turnaround);
+
+    return new Date(resultDate).toLocaleString('hu-Hu');
+};
+
+const addWorkingDays = (submitDate, turnaround) => {
+    let addendWorkingDays = getWorkingDays(turnaround);
+    let resultDate = submitDate;
+
+    while (addendWorkingDays > 0) {
+        const dayIndex = resultDate.getDay();
+        resultDate = addDays(resultDate, addendWorkingDays);
+        addendWorkingDays = Math.floor((dayIndex + addendWorkingDays) / DAYS_IN_WEEK) * 2;
+    }
+
+    return addDays(resultDate, getWeekendCorrection(resultDate));
+}
+
+export const addRemainingWorkingHours = (date, turnaround) => {
+    const hours = getRemainingHours(turnaround);
+    let result = new Date(date);
+    const endHour = result.getHours() + hours;
+    result.setHours(endHour < END_OF_WORK ? endHour : endHour + DAILY_NON_WORKING_HOURS);
+
+    return addDays(result, getWeekendCorrection(result));
+}
+
+const getWorkingDays = (hours) => {
+    return Math.floor(hours / DAILY_WORKING_HOURS);
+}
+
+const getRemainingHours = (turnaround) => {
+    const workingDays = getWorkingDays(turnaround)
+    return (turnaround === 0 || workingDays === 0) ? turnaround : turnaround % workingDays;
+};
+
+const getWeekendCorrection = (date) => {
+    switch (date.getDay()) {
+        case 0:
+            return 1;
+        case 6:
+            return 2;
+        default:
+            return 0;
+    }
+}
+
+const addDays = (date, days) => {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+export const validateArguments = (date, turnaround) => {
+    if ([undefined, null, ''].includes(date)) {
+        throw new Error('Date is required')
+    }
+    if (!date instanceof Date || isNaN(date)) {
+        throw new Error('Date is invalid')
+    }
+    if ([undefined, null, ''].includes(turnaround)) {
+        throw new Error('Turnaround is required')
+    }
+    if (typeof turnaround !== 'number' || isNaN(turnaround)) {
+        throw new Error('Turnaround must be number')
+    }
+    return true
+}
---
+++ index.test.js
@@ -0,0 +1,71 @@
+import {calculateDueDate, validateArguments, addRemainingWorkingHours} from "./index";
+
+
+describe('#tests', () => {
+
+    describe('#dueDateCalculation', () => {
+        const testCases = [
+            { submitDate: new Date('2023-08-11 13:21:17'), turnaround: 120, expectedResult: '2023. 09. 01. 13:21:17' },
+            { submitDate: new Date('2023-10-06 13:21:00'), turnaround: 0, expectedResult: '2023. 10. 06. 13:21:00' },
+            { submitDate: new Date('2023-10-06 13:21:00'), turnaround: 1, expectedResult: '2023. 10. 06. 14:21:00' },
+            { submitDate: new Date('2023-10-06 13:21:00'), turnaround: 4, expectedResult: '2023. 10. 09. 9:21:00' },
+            { submitDate: new Date('2023-08-31 16:59:00'), turnaround: 1, expectedResult: '2023. 09. 01. 9:59:00' },
+            { submitDate: new Date('2023-11-06 9:00:00'), turnaround: 320, expectedResult: '2024. 01. 01. 9:00:00' },
+            { submitDate: new Date('2024-01-01 9:00:00'), turnaround: 2120, expectedResult: '2025. 01. 06. 9:00:00' },
+        ];
+        testCases.forEach(({ submitDate, turnaround, expectedResult }) => {
+            test(`${submitDate} submit date with ${turnaround} turnaround`, () => {
+                expect(calculateDueDate(submitDate, turnaround))
+                    .toBe(expectedResult);
+            });
+        });
+    });
+
+    describe('#validation', () => {
+
+        test('throwing invalid date error', () => {
+            expect(() => validateArguments('asd', 1))
+                .toThrow(new Error('Date is invalid'));
+        });
+
+        test('throwing invalid date error', () => {
+            expect(() => validateArguments(new Date('asd'), 1))
+                .toThrow(new Error('Date is invalid'));
+        });
+
+        test('throwing required date error', () => {
+            expect(() => validateArguments('', 1))
+                .toThrow(new Error('Date is required'));
+        });
+
+        test('throwing required turnaround error', () => {
+            expect(() => validateArguments(new Date(), null))
+                .toThrow(new Error('Turnaround is required'));
+        });
+
+        test('throwing turnaround must be a number', () => {
+            expect(() => validateArguments(new Date(), true))
+                .toThrow(new Error('Turnaround must be number'));
+        });
+    });
+
+    describe('#remainingWorkingHoursAddition', () => {
+        const testCases = [
+            {date: new Date('2023-08-10 13:21:00'), hours: 3, expected: '2023. 08. 10. 16:21:00'},
+            {date: new Date('2023-08-10 13:21:00'), hours: 4, expected: '2023. 08. 11. 9:21:00'},
+            {date: new Date('2023-08-11 13:21:00'), hours: 4, expected: '2023. 08. 14. 9:21:00'}
+        ];
+
+        testCases.forEach(({date, hours, expected}) => {
+            test(`adding ${hours} hour(s) to ${date.toLocaleString('hu-Hu')}`, () => {
+                const result = addRemainingWorkingHours(date, hours)
+                expect(result.toLocaleString('hu-Hu')).toBe(expected);
+            });
+        })
+    })
+})
+
+
+
+
+
